7112423a842967c81f4d133839e8650d4c24c771
Rémi Berson <remi.berson@gmail.com>
2013-04-30 13:12:06 +0200

    Create repo

---
+++ Math/HKMeans.hs
@@ -0,0 +1,69 @@
+module Math.HKMeans(kmeans) where
+
+import Prelude hiding
+    (all,
+    cycle,
+    head,
+    iterate,
+    last,
+    length,
+    map,
+    sum,
+    take,
+    zip,
+    (++))
+import Numeric.LinearAlgebra
+import Data.List.Stream
+import Data.Ord
+
+type Distance = (Vector Double -> Vector Double -> Double)
+
+-- Cluster the datas into k parts
+initClustering :: Int -> [Int]
+initClustering k = cycle [1..k]
+{-# INLINE initClustering #-}
+
+
+-- Compute the centroids of each cluster
+getCentroids :: Int -> ([Int], [Vector Double]) -> [Vector Double]
+getCentroids k (ids, vectors) = let items = zip ids vectors in map (getCentroid items) [1..k]
+    where
+        -- Sum the vectors that have the same cluster id and divide the sum to get the mean
+        getCentroid ::  [(Int, Vector Double)] -> Int -> Vector Double
+        getCentroid items cid = let (acc, s) = sumVectors items cid
+            in s / fromIntegral acc
+        -- Sum the vectors that are in the cluster `cid`
+        sumVectors :: [(Int, Vector Double)] -> Int -> (Int, Vector Double)
+        sumVectors [] _ = (0, 0)
+        sumVectors ((c, vec):t) cid
+            | c == cid   = let (acc, v) = sumVectors t cid in (acc + 1, v + vec)
+            | otherwise = sumVectors t cid
+
+
+-- Return the closest Centroid from the given vector
+closest :: Distance -> [(Int, Vector Double)] -> Vector Double -> Int
+closest d centroids vec = fst $ minimumBy (comparing (d vec . snd)) centroids
+{-# INLINE closest #-}
+
+
+-- Cluster items
+clusterize :: Distance -> [Vector Double] -> [Vector Double] -> [Int]
+clusterize d centroids datas = [c | c <- map (closest d $ zip [1..] centroids) datas]
+{-# INLINE clusterize #-}
+
+
+-- KMeans naive algorithm
+kmeans :: Distance -> Int -> [Vector Double] -> ([Int], [Vector Double])
+kmeans _ 0 _ = error "Clustering with 0 cluster is impossible"
+kmeans _ _ [] = ([], [])
+kmeans _ 1 datas = (take (length datas) $ initClustering 1, datas)
+kmeans distance k datas =
+    let clustering = initClustering k -- Init the items with random cluster id
+        centroids = getCentroids k (clustering, datas) -- Init the centroids
+    in (kmeans' centroids clustering, datas)
+    where
+        kmeans' :: [Vector Double] -> [Int] -> [Int]
+        kmeans' centroids ids
+            | ids == newIds = ids
+            | otherwise     = kmeans' (getCentroids k (ids, datas)) newIds
+            where newIds = clusterize distance centroids datas
---
+++ Test/KMeansCheck.hs
@@ -0,0 +1,52 @@
+
+
+module Main where
+
+import Math.HKMeans
+
+import Control.Monad
+import System.Environment
+import System.Random
+import Data.Packed.Vector
+import Numeric.LinearAlgebra
+
+-- Cosine distance used by the KMeans algorithm
+cosine :: Vector Double -> Vector Double -> Double
+cosine d1 d2 = (d1 <.> d2) / (norm1 d1) * (norm1 d2)
+
+-- Euclidian distance
+edistance :: Vector Double -> Vector Double -> Double
+edistance d1 d2 = norm1 (d1 - d2)
+
+
+
+main :: IO ()
+main = let datas = (map fromList [[1], [3], [5], [42], [43], [44], [100], [105]]) in
+    do
+        test 1 datas
+        test 2 datas
+        test 3 datas
+        test 4 datas
+        test 5 datas
+    where
+        test k datas = print $ kmeans edistance k datas
+
+-- Benchmarking my 'kmeans-vector' package
+
+{-
+main :: IO ()
+main = do
+  [dimension, nb, k] <- getArgs
+  go dimension nb k 10
+  where go :: String -> String -> String -> Int -> IO ()
+        go _ _ _ 0 = return ()
+        go d nb k j = do
+          vectors <- generateVecs (read nb :: Int) (read d :: Int)
+          let cls = kmeans edistance (read k :: Int) vectors
+          putStrLn . show $ cls
+          go d nb k (j - 1)
+
+generateVecs :: Int -> Int -> IO [Vector Double]
+generateVecs nb d = do
+  replicateM nb (fromList `fmap` replicateM d (randomRIO (-10, 10)))
+-}
